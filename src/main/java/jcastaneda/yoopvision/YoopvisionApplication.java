d8123d7171157f5cbdbad6c9e52d1bde0320fc70
Jowel Castaneda <jowelcastaneda6@gmail.com>
2023-01-31 02:14:41 +0800

    Initial commit

---
+++ src/main/java/jcastaneda/yoopvision/YoopvisionApplication.java
@@ -0,0 +1,13 @@
+package jcastaneda.yoopvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoopvisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoopvisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jcastaneda/yoopvision/YoopvisionApplicationTests.java
@@ -0,0 +1,13 @@
+package jcastaneda.yoopvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoopvisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
